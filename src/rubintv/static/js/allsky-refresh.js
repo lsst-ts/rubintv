eb4e85aaea7ad81a8226138c7eb09b5ce15c3d52
ugyballoons <ugyballoons@gmail.com>
2022-05-01 00:53:07 +0100

    allsky js bugfix and check for new video at end of play

--- src/rubintv/static/js/allsky-refresh.js
+++ src/rubintv/static/js/allsky-refresh.js
@@ -1,29 +1,9 @@
 (function($){
-  // setInterval(function refreshImage(){
-  //   let url_path = document.location.pathname;
-  //   $('.current-still').load(url_path + "/update/image")
-  // }, 5000);
+  let url_path = document.location.pathname;
 
   setInterval(function refresh(){
-    let url_path = document.location.pathname;
-
-    $.get(url_path + "/update/monitor", function(data){
-      let currentMovie = $('.current-movie')
-      let currentMovieUrl = currentMovie.find('video source').attr('src');
-
-      if (data.eventType == "monitor" && data.src != currentMovieUrl) {
-        currentMovie.find('video source').attr({'src': data.src});
-        currentMovie.find('.subheader h3').text(`${data.date} ${data.seq}`);
-        currentMovie.find('.desc').text(data.name);
-        currentMovie.find('video')[0].load();
-        console.log('new movie found');
-      } else {
-        console.log('same movie');
-      }
-    });
-
     $.get(url_path + "/update/image", function(data){
-      if (data.eventType == "image"){
+      if (data.channel == "image"){
         let currentImage = $('.current-still');
         currentImage.find('img').attr({'src': data.src})
         currentImage.find('.subheader h3').text(`${data.date} : Sequence ${data.seq}`);
@@ -32,5 +12,22 @@
     });
   }, 5000);
 
+  let onEnded = function() {
+    const video = this;
+    $.get(url_path + "/update/monitor", function(data){
+    let currentMovie = $('.current-movie')
+    let currentMovieUrl = $(video).find('source').attr('src');
+    if (data.channel == "monitor" && data.src != currentMovieUrl) {
+      $(video).find('source').attr({'src': data.src});
+      currentMovie.find('.subheader h3').text(`${data.date} ${data.seq}`);
+      currentMovie.find('.desc').text(data.name);
+      video.load();
+      } else {
+        video.play()
+      }
+    });
+  }
+
+  $("video").on("ended", onEnded);
 
 })(jQuery)
