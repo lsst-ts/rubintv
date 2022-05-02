0b997f23531064a6bdfb63e8a1b2a1f68a3a5b8c
ugyballoons <ugyballoons@gmail.com>
2022-05-02 15:22:19 +0100

    Fix allsky movie check bug, also refresh every 5 secs

--- src/rubintv/static/js/allsky-refresh.js
+++ src/rubintv/static/js/allsky-refresh.js
@@ -1,33 +1,31 @@
 (function($){
   let url_path = document.location.pathname;
+  let currentImage = $('.current-still');
+  let currentMovie = $('.current-movie')
 
   setInterval(function refresh(){
     $.get(url_path + "/update/image", function(data){
       if (data.channel == "image"){
-        let currentImage = $('.current-still');
-        currentImage.find('img').attr({'src': data.src})
+        currentImage.find('img').attr({'src': data.url})
         currentImage.find('.subheader h3').text(`${data.date} : Sequence ${data.seq}`);
         currentImage.find('.desc').text(data.name)
       }
     });
   }, 5000);
 
-  let onEnded = function() {
-    const video = this;
+  let videoCheckLatest = function() {
+    video = currentMovie.find("video")[0];
     $.get(url_path + "/update/monitor", function(data){
-    let currentMovie = $('.current-movie')
     let currentMovieUrl = $(video).find('source').attr('src');
-    if (data.channel == "monitor" && data.src != currentMovieUrl) {
-      $(video).find('source').attr({'src': data.src});
+    if (data.channel == "monitor" && data.url != currentMovieUrl) {
+      $(video).find('source').attr({'src': data.url});
       currentMovie.find('.subheader h3').text(`${data.date} ${data.seq}`);
       currentMovie.find('.desc').text(data.name);
       video.load();
-      } else {
-        video.play()
       }
     });
   }
 
-  $("video").on("ended", onEnded);
+  setInterval(videoCheckLatest, 5000);
 
 })(jQuery)
