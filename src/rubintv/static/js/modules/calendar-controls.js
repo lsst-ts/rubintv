cb89ffcb066c6da790d7f0e90ceb73c6c5ae2fc3
ugyballoons <ugyballoons@gmail.com>
2022-09-12 16:32:52 +0100

    Allow more than one calendar to be open

--- src/rubintv/static/js/modules/calendar-controls.js
+++ src/rubintv/static/js/modules/calendar-controls.js
@@ -2,12 +2,6 @@
 
 export function applyYearControls () {
   $('.year-title').click(function () {
-    const $yearEl = $(this).parent('.year')
-    if ($yearEl.hasClass('open')) {
-      $yearEl.removeClass('open').addClass('closed')
-    } else {
-      $('.year.open').removeClass('open').addClass('closed')
-      $yearEl.removeClass('closed').addClass('open')
-    }
+    $(this).parent('.year').toggleClass('open').toggleClass('closed')
   })
 }
