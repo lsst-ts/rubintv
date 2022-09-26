6fe79d3cf1ad73260f18b5beffcd1a94672d64f2
ugyballoons <ugyballoons@gmail.com>
2022-09-26 21:55:01 +0100

    Remove unnecessary updating of all heartbeats

--- src/rubintv/static/js/admin-status.js
+++ src/rubintv/static/js/admin-status.js
@@ -2,20 +2,6 @@
 import { ChannelStatus } from './modules/heartbeat.js'
 
 (function ($) {
-  const UPDATE_ALL_AFTER = 6000
-
   const services = Array.from(document.querySelectorAll('.service').values()).map(s => s.id)
-  let stats = services.map(s => new ChannelStatus(s))
-
-  setInterval(() => {
-    const urlPath = document.location.pathname
-    $.get(`${urlPath}/heartbeats`, (newHeartbeats) => {
-      console.log('Updating all heartbeats')
-      newHeartbeats.forEach(hb => {
-        console.log(`Found ${hb.channel}`)
-      })
-      stats.forEach(service => { service.displayStatus(false) })
-      stats = newHeartbeats.map(hb => new ChannelStatus(hb.channel))
-    })
-  }, UPDATE_ALL_AFTER * 1000)
+  services.map(s => new ChannelStatus(s))
 })(jQuery)
