c76248a83a9cd6fb0784ea10c03a4c916707099e
ugyballoons <ugyballoons@gmail.com>
2022-11-05 13:24:00 +0000

    Update endpoints __all__s

--- src/rubintv/handlers/external/endpoints.py
+++ src/rubintv/handlers/external/endpoints.py
@@ -2,7 +2,18 @@
 
 __all__ = [
     "get_page",
+    "get_admin_page",
+    "reload_historical",
+    "request_heartbeat_for_channel",
+    "request_all_heartbeats",
+    "get_all_sky_current",
+    "get_all_sky_current_update",
+    "get_allsky_historical",
+    "get_allsky_historical_movie",
     "get_recent_table",
+    "update_todays_table",
+    "get_historical",
+    "get_historical_day_data",
     "events",
     "current",
 ]
