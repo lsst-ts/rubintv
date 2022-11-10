491b97327ea78ce7866e9ab11fc42ae04c956c5e
ugyballoons <ugyballoons@gmail.com>
2022-11-10 17:11:57 +0000

    Add endpoints tests

--- tests/handlers/external/endpoints_test.py
+++ tests/handlers/external/endpoints_test.py
@@ -2,9 +2,11 @@
 
 from __future__ import annotations
 
+import json
 from typing import TYPE_CHECKING
 
 from rubintv.app import create_app
+from rubintv.models.models import get_current_day_obs
 
 if TYPE_CHECKING:
     from aiohttp.pytest_plugin.test_utils import TestClient
@@ -18,6 +20,125 @@ async def test_get_index(aiohttp_client: TestClient) -> None:
 
     response = await client.get(f"/{name}/")
     assert response.status == 200
+
+
+async def test_admin_page(aiohttp_client: TestClient) -> None:
+    """Test GET /app-name/admin"""
+    app = create_app()
+    name = app["safir/config"].name
+    client = await aiohttp_client(app)
+
+    response = await client.get(f"/{name}/admin")
+    assert response.status == 200
+
+
+async def test_reload_historical(aiohttp_client: TestClient) -> None:
+    """Test GET /app-name/reload_historical
+    Reloads all historical data from the bucket"""
+    app = create_app()
+    name = app["safir/config"].name
+    client = await aiohttp_client(app)
+
+    response = await client.get(f"/{name}/reload_historical")
+    assert response.status == 200 and response.text == "OK"
+
+
+async def request_heartbeat_for_auxtel_monitor(
+    aiohttp_client: TestClient,
+) -> None:
+    """Test GET /app-name/heartbeat/auxtel_monitor"""
+    app = create_app()
+    name = app["safir/config"].name
+    client = await aiohttp_client(app)
+
+    response = await client.get(f"/{name}/heartbeat/auxtel_monitor")
+    assert response.status == 200 and json.loads(response.body)
+
+
+async def request_all_heartbeats(aiohttp_client: TestClient) -> None:
+    """Test GET /app-name/heartbeats"""
+    app = create_app()
+    name = app["safir/config"].name
+    client = await aiohttp_client(app)
+
+    response = await client.get(f"/{name}/heartbeats")
+    assert response.status == 200 and json.loads(response.body)
+
+
+async def request_heartbeat_for_unknown_channel(
+    aiohttp_client: TestClient,
+) -> None:
+    """Test GET /app-name/heartbeat/none-existant"""
+    app = create_app()
+    name = app["safir/config"].name
+    client = await aiohttp_client(app)
+
+    response = await client.get(f"/{name}/heartbeat/non-existant")
+    assert response.status == 404
+
+
+async def test_get_allsky_page(aiohttp_client: TestClient) -> None:
+    """Test GET /app-name/allsky
+    All Sky has its own page not based on the general camera
+    page template or endpoint"""
+    app = create_app()
+    name = app["safir/config"].name
+    client = await aiohttp_client(app)
+
+    response = await client.get(f"/{name}/allsky")
+    assert response.status == 200
+
+
+async def test_get_allsky_image_update(aiohttp_client: TestClient) -> None:
+    """Test GET /app-name/allsky/image"""
+    app = create_app()
+    name = app["safir/config"].name
+    client = await aiohttp_client(app)
+
+    response = await client.get(f"/{name}/allsky/image")
+    assert response.status == 200 and json.loads(response.body)
+
+
+async def test_get_allsky_movie_update(aiohttp_client: TestClient) -> None:
+    """Test GET /app-name/allsky/movie"""
+    app = create_app()
+    name = app["safir/config"].name
+    client = await aiohttp_client(app)
+
+    response = await client.get(f"/{name}/allsky/movie")
+    assert response.status == 200 and json.loads(response.body)
+
+
+async def test_get_allsky_historical(aiohttp_client: TestClient) -> None:
+    """Test GET /app-name/allsky/historical"""
+    app = create_app()
+    name = app["safir/config"].name
+    client = await aiohttp_client(app)
+
+    response = await client.get(f"/{name}/allsky/historical")
+    assert response.status == 200
+
+
+async def test_get_allsky_movie_for_date(aiohttp_client: TestClient) -> None:
+    """Test GET /app-name/allsky/historical/2022-11-09"""
+    app = create_app()
+    name = app["safir/config"].name
+    client = await aiohttp_client(app)
+
+    response = await client.get(f"/{name}/allsky/historical/2022-11-09")
+    assert response.status == 200
+
+
+async def test_get_allsky_movie_for_badly_formed_date(
+    aiohttp_client: TestClient,
+) -> None:
+    """Test GET /app-name/allsky/historical/111-111-111"""
+    app = create_app()
+    name = app["safir/config"].name
+    client = await aiohttp_client(app)
+
+    response = await client.get(f"/{name}/allsky/historical/111-111-111")
+    assert response.status == 404
 
 
 async def test_get_camera_page(aiohttp_client: TestClient) -> None:
@@ -38,6 +159,37 @@ async def test_get_camera_page_fail(aiohttp_client: TestClient) -> None:
 
     response = await client.get(f"/{name}/none")
     # Expect this to fail since "none" is not a camera endpoint/slug
+    assert response.status == 404
+
+
+async def test_get_camera_update_for_date(aiohttp_client: TestClient) -> None:
+    """Test GET /app-name/auxtel/update/{today}
+    Should respond with two-part json object with keys 'table'
+    and 'per-day'"""
+    app = create_app()
+    name = app["safir/config"].name
+    client = await aiohttp_client(app)
+
+    day_obs = get_current_day_obs()
+    # date object prints as YYYY-MM-DD
+    response = await client.get(f"/{name}/update/{day_obs}")
+
+    json_res = json.loads(response.body)
+
+    assert response.status == 200 and (
+        "table" in json_res and "per_day" in json_res
+    )
+
+
+async def test_camera_update_for_badly_formed_date(
+    aiohttp_client: TestClient,
+) -> None:
+    """Test GET /app-name/auxtel/update/111-111-111"""
+    app = create_app()
+    name = app["safir/config"].name
+    client = await aiohttp_client(app)
+
+    response = await client.get(f"/{name}/auxtel/update/111-111-111")
     assert response.status == 404
 
 
@@ -95,3 +247,15 @@ async def test_get_camera_historical_date(aiohttp_client: TestClient) -> None:
 
     response = await client.get(f"/{name}/auxtel/historical/2022-02-23")
     assert response.status == 200
+
+
+async def test_get_camera_historical_for_badly_formed_date(
+    aiohttp_client: TestClient,
+) -> None:
+    """Test GET /app-name/auxtel/historical/111-111-111"""
+    app = create_app()
+    name = app["safir/config"].name
+    client = await aiohttp_client(app)
+
+    response = await client.get(f"/{name}/auxtel/historical/111-111-111")
+    assert response.status == 404
