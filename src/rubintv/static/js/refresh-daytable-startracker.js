761aa11ed3ad5bc39ad086dd6ff8f2ea49884add
ugyballoons <ugyballoons@gmail.com>
2022-11-12 15:41:36 +0000

    Fix Star Tracker redraw issue

--- src/rubintv/static/js/refresh-daytable-startracker.js
+++ src/rubintv/static/js/refresh-daytable-startracker.js
@@ -6,7 +6,7 @@ import { startrackerDefaultSelected } from './models.js'
 
 (function ($) {
   function starTrackerHtmlInject (htmlParts) {
-    $('#per-day-refreshable').html(htmlParts.per_day)
+    $('.channel-day-data').html(htmlParts.table)
   }
 
   initTable(starTrackerHtmlInject, 5, startrackerDefaultSelected)
