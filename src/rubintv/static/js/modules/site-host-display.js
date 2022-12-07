ba0ade9fbad8f72b7c8a12c433bcbf43035ae9a0
ugyballoons <ugyballoons@gmail.com>
2022-12-07 18:07:05 +0000

    Add colour variation to site-loc display

--- src/rubintv/static/js/modules/site-host-display.js
+++ src/rubintv/static/js/modules/site-host-display.js
@@ -9,6 +9,7 @@
   if (display.length > 0) {
     const displayEl = document.createElement('div')
     displayEl.className = 'site-host-display'
+    display.forEach((c) => displayEl.classList.add(c))
     const text = document.createTextNode(display.join(' '))
     displayEl.appendChild(text)
     document.body.append(displayEl)
