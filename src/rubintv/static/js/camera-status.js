09bf37010cc6c55ddc306c2f4e49e1c58b20bc27
ugyballoons <ugyballoons@gmail.com>
2022-12-14 22:23:21 +0000

    Fix camera page status not showing bug

--- src/rubintv/static/js/camera-status.js
+++ src/rubintv/static/js/camera-status.js
@@ -1,19 +1,28 @@
 import { ChannelStatus } from './modules/heartbeat.js'
 
 window.addEventListener('DOMContentLoaded', function () {
-  const services = Array.from(document.querySelectorAll('.service')
-    .values())
-    // eslint-disable-next-line no-new-object
-    .map(s => new Object({ id: s.id, dependentOn: s.dataset.dependentOn }))
-  const dependenciesNames = Array.from(new Set(services.map(s => s.dependentOn)))
-    // eslint-disable-next-line eqeqeq
-    .filter(d => !(d === '' || typeof d === 'undefined'))
+  const services = Array.from(document.querySelectorAll('.service'))
+    .map(s => {
+      return { id: s.id, dependentOn: s.dataset.dependentOn }
+    })
 
-  const dependencies = Object.fromEntries(
-    dependenciesNames.map(d => [d,
-      new ChannelStatus(d)])
+  // boil down dependency names
+  const dependenciesNames = Array.from(
+    new Set(
+      services.map(service => { return service.dependentOn })
+        // filter out any undefined or empty dependencies
+        .filter(serviceName => { return serviceName })
+    )
   )
 
-  services
-    .map(s => new ChannelStatus(s.id, dependencies[s.dependentOn]))
+  const dependencies = Object.fromEntries(
+    dependenciesNames.map(d => {
+      return [d, new ChannelStatus(d)]
+    }
+    )
+  )
+
+  services.map(s => {
+    return new ChannelStatus(s.id, dependencies[s.dependentOn])
+  })
 })
