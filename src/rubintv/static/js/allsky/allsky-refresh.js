33c11e7812dce7c436933a7864d283f1cc527ce9
ugyballoons <ugyballoons@gmail.com>
2022-12-15 18:14:10 +0000

    Use hash for class names in table

--- src/rubintv/static/js/allsky/allsky-refresh.js
+++ src/rubintv/static/js/allsky/allsky-refresh.js
@@ -7,29 +7,31 @@ window.addEventListener('load', () => {
   const currentMovie = document.querySelector('.current-movie')
 
   setInterval(function refresh () {
-    getJson(urlPath + '/update/image').then(data => {
-      if (data.channel === 'image') {
-        currentImage.querySelector('img').setAttribute('src', data.url)
-        currentImage.querySelector('a').setAttribute('href', data.url)
-        currentImage.querySelector('.subheader h3').textContent = `${data.date} : Image ${data.seq}`
-        currentImage.querySelector('.desc').textContent = data.name
-      }
-    })
+    getJson(urlPath + '/update/image')
+      .then(data => {
+        if (data.channel === 'image') {
+          currentImage.querySelector('img').setAttribute('src', data.url)
+          currentImage.querySelector('a').setAttribute('href', data.url)
+          currentImage.querySelector('.subheader h3').textContent = `${data.date} : Image ${data.seq}`
+          currentImage.querySelector('.desc').textContent = data.name
+        }
+      })
   }, 5000)
 
   const videoCheckLatest = function () {
     const video = currentMovie.querySelector('video')
-    getJson(urlPath + '/update/movie').then(data => {
-      const source = video.querySelector('source')
-      const currentMovieUrl = source.getAttribute('src')
-      if (data.channel === 'movie' && data.url !== currentMovieUrl) {
-        source.setAttribute('src', data.url)
-        currentMovie.querySelector('.movie-date').textContent(data.date)
-        currentMovie.querySelector('.movie-number').textContent(data.seq)
-        currentMovie.querySelector('.desc').textContent(data.name)
-        video.load()
-      }
-    })
+    getJson(urlPath + '/update/movie')
+      .then(data => {
+        const source = video.querySelector('source')
+        const currentMovieUrl = source.getAttribute('src')
+        if (data.channel === 'movie' && data.url !== currentMovieUrl) {
+          source.setAttribute('src', data.url)
+          currentMovie.querySelector('.movie-date').textContent(data.date)
+          currentMovie.querySelector('.movie-number').textContent(data.seq)
+          currentMovie.querySelector('.desc').textContent(data.name)
+          video.load()
+        }
+      })
   }
   setInterval(videoCheckLatest, 5000)
 
