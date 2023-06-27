0eca88bfd7735aa6ab5648a31d45f53cbf25cbc1
ugyballoons <ugyballoons@gmail.com>
2023-06-27 15:46:23 +0100

    Start afresh

--- src/js/pages/night_report_historical.js
+++
@@ -1,6 +0,0 @@
-import { addTabsListeners, listenForKeypresses } from '../night-report/tabs-ui.js'
-
-window.addEventListener('DOMContentLoaded', () => {
-  addTabsListeners()
-  listenForKeypresses()
-})
