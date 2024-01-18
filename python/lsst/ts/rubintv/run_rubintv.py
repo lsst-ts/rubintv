c98fc6e2b2fbe2518637d4dce8964b5438bf04c0
Sebastian Aranda Sanchez <aranda.sebastian@gmail.com>
2024-01-18 13:50:25 -0300

    Fix reference to ASGI app

--- python/lsst/ts/rubintv/run_rubintv.py
+++ python/lsst/ts/rubintv/run_rubintv.py
@@ -1,8 +1,9 @@
 import uvicorn
+from lsst.ts.rubintv.main import app
 
 
 def run_rubintv() -> None:
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="info")
+    uvicorn.run(app, host="127.0.0.1", port=8000, log_level="info")
 
 
 if __name__ == "__main__":
