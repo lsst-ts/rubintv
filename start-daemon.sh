875fc9773b4a3ea5e3d77cfeba3f407e64119732
ugyballoons <ugyballoons@gmail.com>
2024-05-13 16:29:08 +0100

    Make changes for PR.
    Add docstrings to DDV websocket handler.
    Complete existing merge.
    Address required dir change in bash script.

--- start-daemon.sh
+++ start-daemon.sh
@@ -5,9 +5,15 @@ if ! command -v flutter &> /dev/null
 then
     echo "Error: Flutter not installed" >&2
 else
-    echo -e "ADDRESS=<hostname>\nPORT=8080" > .env
-    flutter build web
-    cp build/web ddv
+    if [ ! -d "rubintv_visualization" ]
+    then
+        echo "Error: directory rubintv_visualization doesn't exist"
+    else
+        cd rubintv_visualization
+        echo -e "ADDRESS=<hostname>\nPORT=8080" > .env
+        flutter build web
+        cp build/web ../ddv
+    fi
 fi
 
 run_rubintv
