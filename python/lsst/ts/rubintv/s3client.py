49cfe7700950ef36f614480f9991a0814420dfef
ugyballoons <ugyballoons@gmail.com>
2024-07-05 16:14:01 +0100

    Refactor S3Client

--- python/lsst/ts/rubintv/s3client.py
+++ python/lsst/ts/rubintv/s3client.py
@@ -1,7 +1,7 @@
 import json
 from typing import Any
 
-from aiobotocore.session import AioSession
+from aiobotocore.session import AioBaseClient, AioSession
 from botocore.exceptions import ClientError
 from botocore.response import StreamingBody
 from fastapi.exceptions import HTTPException
@@ -16,36 +16,32 @@ __all__ = ["S3Client"]
 class S3Client:
     def __init__(self, profile_name: str, bucket_name: str) -> None:
         self.endpoint_url = app_config.s3_endpoint_url
-        self.session = AioSession(profile=profile_name)
+        self._session = AioSession(profile=profile_name)
         self._bucket_name = bucket_name
+        self._client = None
+
+    async def _get_client(self) -> AioBaseClient:
+        if not self._client:
+            async with self._session.create_client(
+                "s3", endpoint_url=self.endpoint_url, region_name="us-east-1"
+            ) as client:
+                self._client = client
+        return self._client
 
     async def async_list_objects(self, prefix: str) -> list[dict[str, str]]:
-        async with self.session.create_client(
-            "s3", endpoint_url=self.endpoint_url, region_name="us-east-1"
-        ) as client:
+        async with await self._get_client() as client:
             objects = []
-            response = await client.list_objects_v2(
+            paginator = client.get_paginator("list_objects_v2")
+            async for page in paginator.paginate(
                 Bucket=self._bucket_name, Prefix=prefix
-            )
-            while True:
-                for content in response.get("Contents", []):
-                    object = {}
-                    object["key"] = content["Key"]
-                    object["hash"] = content["ETag"].strip('"')
-                    objects.append(object)
-                if "NextContinuationToken" not in response:
-                    break
-                response = await client.list_objects_v2(
-                    Bucket=self._bucket_name,
-                    Prefix=prefix,
-                    ContinuationToken=response["NextContinuationToken"],
-                )
+            ):
+                for content in page.get("Contents", []):
+                    obj = {"key": content["Key"], "hash": content["ETag"].strip('"')}
+                    objects.append(obj)
             return objects
 
     async def async_get_object(self, key: str) -> dict[str, Any]:
-        async with self.session.create_client(
-            "s3", endpoint_url=self.endpoint_url, region_name="us-east-1"
-        ) as client:
+        async with await self._get_client() as client:
             try:
                 obj = await client.get_object(Bucket=self._bucket_name, Key=key)
                 str_data = await obj["Body"].read()
@@ -56,13 +52,11 @@ class S3Client:
                 return data
             except ClientError as e:
                 if e.response["Error"]["Code"] == "NoSuchKey":
-                    logger.info("Object for key: {key} not found.", key=key)
+                    logger.info(f"Object for key: {key} not found.")
                 return {}
 
     async def get_raw_object(self, key: str) -> StreamingBody:
-        async with self.session.create_client(
-            "s3", endpoint_url=self.endpoint_url, region_name="us-east-1"
-        ) as client:
+        async with await self._get_client() as client:
             try:
                 obj = await client.get_object(Bucket=self._bucket_name, Key=key)
                 data = obj["Body"]
@@ -72,9 +66,7 @@ class S3Client:
                 raise HTTPException(status_code=404, detail=f"No such file for: {key}")
 
     async def get_movie(self, key: str, headers: dict[str, str] | None = None) -> Any:
-        async with self.session.create_client(
-            "s3", endpoint_url=self.endpoint_url, region_name="us-east-1"
-        ) as client:
+        async with await self._get_client() as client:
             try:
                 data = await client.get_object(
                     Bucket=self._bucket_name, Key=key, **(headers or {})
