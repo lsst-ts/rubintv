82dff68249b600c6d3ce29e6fb9c54a410817b7f
Sebastian Aranda Sanchez <aranda.sebastian@gmail.com>
2024-07-09 12:22:21 -0400

    Connect environment variables for flutter build script

--- scripts/build-flutter-app.sh
+++ scripts/build-flutter-app.sh
@@ -5,16 +5,14 @@ set -e
 # Display each command as it's run.
 set -x
 
-DEPLOY_BRANCH=deploy-slac
-
 cd /usr/src/rubintv
-git clone --single-branch --branch $DEPLOY_BRANCH https://github.com/lsst-sitcom/rubin_chart
-git clone --single-branch --branch $DEPLOY_BRANCH https://github.com/lsst-ts/rubintv_visualization ./ddv
+git clone --single-branch --branch $DDV_DEPLOY_BRANCH https://github.com/lsst-sitcom/rubin_chart
+git clone --single-branch --branch $DDV_DEPLOY_BRANCH https://github.com/lsst-ts/rubintv_visualization ./ddv
 
 # Base HREF must be bookended by "/".
-DDV_BASE_HREF=/rubintv/ddv/
-CLIENT_WS_ADDRESS=rubintv/ws/ddv
+DDV_CLIENT_WS_ADDRESS=${DDV_CLIENT_WS_ADDRESS:-rubintv/ws/ddv}
+DDV_BASE_HREF=${DDV_BASE_HREF:-/rubintv/ddv/}
 
 cd ddv
-echo "ADDRESS=$CLIENT_WS_ADDRESS" > .env
+echo "ADDRESS=$DDV_CLIENT_WS_ADDRESS" > .env
 flutter build web --base-href $DDV_BASE_HREF --profile --source-maps
