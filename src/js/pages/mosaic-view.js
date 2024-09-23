9461df70dab88e238188c8914c10ee16477cdd2c
ugyballoons <ugyballoons@gmail.com>
2024-09-23 16:13:26 +0100

    Style and include MosaicView in page

--- src/js/pages/mosaic-view.js
+++ src/js/pages/mosaic-view.js
@@ -1,15 +1,29 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
 import { WebsocketClient } from "../modules/ws-service-client"
+import MosaicView from "../components/MosaicView"
+import { _getById } from '../modules/utils'
 
 (function () {
-    console.log("Am in the function!")
     const locationName = document.documentElement.dataset.locationname
     const camera = window.APP_DATA.camera || {}
     const date = window.APP_DATA.date || ''
     const ws = new WebsocketClient()
+    ws.subscribe('historicalStatus')
     ws.subscribe('service', 'camera', locationName, camera.name)
     camera.mosaic_view_meta.forEach((view) => {
         console.log('Subscribing to:', view.channel)
         ws.subscribe('service', 'channel', locationName, camera.name, view.channel)
     })
+
+    const mosaicRoot = createRoot(_getById('mosaic-view'))
+    mosaicRoot.render(
+    <React.StrictMode>
+        <MosaicView
+          camera={camera}
+          initialDate={date}
+        />
+    </React.StrictMode>
+    )
 })()
 
