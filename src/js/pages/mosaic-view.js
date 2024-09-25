a4d4907d1fc0eca3acb5a7a0740b02e034a36f0c
ugyballoons <ugyballoons@gmail.com>
2024-09-25 16:17:44 +0100

    Add location to app data

--- src/js/pages/mosaic-view.js
+++ src/js/pages/mosaic-view.js
@@ -1,29 +1,29 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
+import React from "react"
+import { createRoot } from "react-dom/client"
 import { WebsocketClient } from "../modules/ws-service-client"
 import MosaicView from "../components/MosaicView"
-import { _getById } from '../modules/utils'
+import { _getById } from "../modules/utils"
 
-(function () {
-    const locationName = document.documentElement.dataset.locationname
-    const camera = window.APP_DATA.camera || {}
-    const date = window.APP_DATA.date || ''
-    const ws = new WebsocketClient()
-    ws.subscribe('historicalStatus')
-    ws.subscribe('service', 'camera', locationName, camera.name)
-    camera.mosaic_view_meta.forEach((view) => {
-        console.log('Subscribing to:', view.channel)
-        ws.subscribe('service', 'channel', locationName, camera.name, view.channel)
-    })
+;(function () {
+  const locationName = window.APP_DATA.locationName || ""
+  const camera = window.APP_DATA.camera || {}
+  const date = window.APP_DATA.date || ""
+  const ws = new WebsocketClient()
+  ws.subscribe("historicalStatus")
+  ws.subscribe("service", "camera", locationName, camera.name)
+  camera.mosaic_view_meta.forEach((view) => {
+    console.log("Subscribing to:", view.channel)
+    ws.subscribe("service", "channel", locationName, camera.name, view.channel)
+  })
 
-    const mosaicRoot = createRoot(_getById('mosaic-view'))
-    mosaicRoot.render(
+  const mosaicRoot = createRoot(_getById("mosaic-view"))
+  mosaicRoot.render(
     <React.StrictMode>
-        <MosaicView
-          camera={camera}
-          initialDate={date}
-        />
+      <MosaicView
+        locationName={locationName}
+        camera={camera}
+        initialDate={date}
+      />
     </React.StrictMode>
-    )
+  )
 })()
-
