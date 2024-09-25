cd316a06aaebdb3f5bbe08182cace2ab9c69e091
ugyballoons <ugyballoons@gmail.com>
2024-09-25 16:19:54 +0100

    Fix table redraw on empty data message

--- src/js/pages/current.js
+++ src/js/pages/current.js
@@ -1,4 +1,4 @@
-import { _getById, _elWithClass } from '../modules/utils'
+import { _getById, _elWithAttrs } from '../modules/utils'
 import { WebsocketClient } from '../modules/ws-service-client'
 
 (function () {
@@ -16,19 +16,20 @@ import { WebsocketClient } from '../modules/ws-service-client'
 
   window.addEventListener('channel', (message) => {
     const newEvent = message.detail.data
-    const filename = newEvent.filename
-    _getById('date').textContent = newEvent.day_obs
-    _getById('seqNum').textContent = newEvent.seq_num
+    if (!newEvent) {
+      return
+    }
+    const { filename, day_obs: dayObs, seq_num: seqNum } = newEvent
+    _getById('date').textContent = dayObs
+    _getById('seqNum').textContent = seqNum
     _getById('eventName').textContent = filename
     const oldImg = _getById('eventImage')
     // create new responsive <img> element
-    const newImg = _elWithClass('img', 'resp')
-    const imgURL = `${baseImgURL}/${filename}`
-    newImg.src = imgURL
-    newImg.id = oldImg.id
+    const imgSrc = new URL(filename, baseImgURL)
+    const newImg = _elWithAttrs('img', {class: 'resp', id: oldImg.id, src: imgSrc})
     newImg.addEventListener('load', () => {
       oldImg.replaceWith(newImg)
-      _getById('eventLink').href = imgURL
+      _getById('eventLink').href = imgSrc
     })
   })
 })()
