969862bf0bc0eefc4c3615aa3f8b4c057643d4d4
ugyballoons <ugyballoons@gmail.com>
2024-12-03 14:43:42 +0000

    Import at root and fix path

--- scripts/install-exp_checker.sh
+++ scripts/install-exp_checker.sh
@@ -5,8 +5,8 @@ set -e
 # Display each command as it's run.
 set -x
 
-cd /usr/src/rubintv/python/lsst/ts
-git clone --single-branch --branch package https://github.com/lsst-sitcom/rubin_exp_checker.git ./exp_checker
+cd /usr/src/rubintv
+git clone --single-branch --branch package https://github.com/lsst-sitcom/rubin_exp_checker.git exp_checker
 cd exp_checker
 pip install -e .
 pip install -r requirements.txt
