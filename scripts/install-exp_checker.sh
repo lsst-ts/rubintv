467c3f94a17f3fcd56d939b1a5d092e901b7298a
ugyballoons <ugyballoons@gmail.com>
2024-12-03 10:12:42 +0000

    Fix import path issues

--- scripts/install-exp_checker.sh
+++ scripts/install-exp_checker.sh
@@ -5,7 +5,7 @@ set -e
 # Display each command as it's run.
 set -x
 
-cd /usr/src/rubintv/lsst/ts
+cd /usr/src/rubintv/python/lsst/ts
 git clone --single-branch --branch package https://github.com/lsst-sitcom/rubin_exp_checker.git ./exp_checker
 cd exp_checker
 pip install -e .
