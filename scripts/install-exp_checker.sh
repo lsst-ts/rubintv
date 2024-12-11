260d3e4545ebcc3eec2f146037294b9c485ac314
ugyballoons <ugyballoons@gmail.com>
2024-12-11 14:43:41 +0000

    Add location check

--- scripts/install-exp_checker.sh
+++ scripts/install-exp_checker.sh
@@ -5,8 +5,11 @@ set -e
 # Display each command as it's run.
 set -x
 
-cd /usr/src/rubintv
-git clone https://github.com/lsst-sitcom/rubin_exp_checker.git exp_checker
-cd exp_checker
-pip install -e .
-pip install -r requirements.txt
+# exp_checker only runs at USDF
+if [ -n "${!RAPID_ANALYSIS_LOCATION}" ] && [ "${!RAPID_ANALYSIS_LOCATION}" == "USDF" ]; then
+    cd /usr/src/rubintv
+    git clone https://github.com/lsst-sitcom/rubin_exp_checker.git exp_checker
+    cd exp_checker
+    pip install -e .
+    pip install -r requirements.txt
+fi
