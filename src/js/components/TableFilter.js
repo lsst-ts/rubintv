f013936fb1530bbb9cc47dd6458152096e101803
ugyballoons <ugyballoons@gmail.com>
2025-03-19 20:49:03 +0000

    Add filter dialog

--- src/js/components/TableFilter.js
+++ src/js/components/TableFilter.js
@@ -1,5 +1,6 @@
-import React from "react"
+import React, { useEffect, useRef } from "react"
 import PropTypes from "prop-types"
+import { useModal } from "./Modal"
 
 export default function TableFilter() {
   return (
@@ -9,11 +10,49 @@ export default function TableFilter() {
   )
 }
 
-export function ColumnFilterInput({ column }) {
-  console.log("In ColumnFilter with column: ", column)
-  const placeholder = `Filter using ${column}...`
-  return <input type="text" placeholder={placeholder} />
+export function FilterDialog({ column, setFilterOn }) {
+  const placeholder = `Enter ${column}...`
+  const inputRef = useRef(null)
+  useEffect(() => {
+    inputRef.current.focus()
+  }, [])
+
+  const { closeModal } = useModal()
+
+  const handleApply = () => {
+    const value = inputRef.current.value
+    setFilterOn({ column, value: value })
+    closeModal()
+  }
+
+  const handleKeyDown = (e) => {
+    console.log(e.key)
+    if (e.key === "Enter") {
+      handleApply()
+    }
+  }
+
+  const handleClear = () => {
+    setFilterOn({ column: "", value: "" })
+    closeModal()
+  }
+
+  return (
+    <>
+      <h2>Filter by {column}</h2>
+      <input
+        ref={inputRef}
+        name="value"
+        type="text"
+        placeholder={placeholder}
+        onKeyDown={handleKeyDown}
+      />
+      <button onClick={handleApply}>Apply</button>
+      <button onClick={handleClear}>Clear</button>
+    </>
+  )
 }
-ColumnFilterInput.propTypes = {
+FilterDialog.propTypes = {
   column: PropTypes.string,
+  setFilterOn: PropTypes.func,
 }
