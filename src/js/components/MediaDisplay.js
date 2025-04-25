1ff142969d9b506e1ef9cb14500fdbb8db92e519
ugyballoons <ugyballoons@gmail.com>
2025-04-25 21:25:39 +0100

    Add docstring and tidy code

--- src/js/components/MediaDisplay.js
+++ src/js/components/MediaDisplay.js
@@ -42,8 +42,9 @@ export default function MediaDisplay({
           <span className="media-date">{mediaEvent.day_obs}</span>
           <span className="media-seqnum">{mediaEvent.seq_num}</span>
         </h2>
-        {!isCurrent && <PrevNext prevNext={prevNext} eventUrl={eventUrl} />}
-        {isCurrent && (
+        {isCurrent ? (
+          <PrevNext prevNext={prevNext} eventUrl={eventUrl} />
+        ) : (
           <TimeSinceLastImageClock metadata={metadata} camera={camera} />
         )}
       </div>
@@ -82,9 +83,19 @@ MediaDisplay.propTypes = {
   isCurrent: PropTypes.bool,
 }
 
+/**
+ *
+ * @param {Object} mEvent
+ * @param {string} imgUrl
+ * @param {string} videoUrl
+ * @returns {Object}
+ * @description This function takes a media event and returns a unified media
+ * event object with the correct media type and source URL.
+ * It determines the media type based on the file extension and constructs
+ * the source URL using the filename and base URL. The media type can be
+ * either "image" or "video".
+ */
 function unifyMediaEvent(mEvent, imgUrl, videoUrl) {
-  // This function is used to unify the media event object with the correct
-  // media type and source URL.
   const { filename, ext } = mEvent
   const mediaType = getMediaType(ext)
   const url = mediaType === "video" ? videoUrl : imgUrl
