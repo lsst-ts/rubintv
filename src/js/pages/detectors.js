86a859822c3c53443c3b1985ecfeaf7ab11a1017
ugyballoons <ugyballoons@gmail.com>
2025-04-30 14:58:32 +0100

    Move step1b cells into one row

---
+++ src/js/pages/detectors.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { createRoot } from "react-dom/client"
+import { _getById } from "../modules/utils"
+import Detector from "../components/Detector"
+import { WebsocketClient } from "../modules/ws-service-client"
+;(function () {
+  const { locationName, camera = {}, date = "", isHistorical } = window.APP_DATA
+
+  // Set up websocket connection if not historical
+  if (!isHistorical) {
+    const ws = new WebsocketClient()
+    ws.subscribe("service", "detector", locationName, camera.name)
+  }
+
+  const tableRoot = createRoot(_getById("detectors"))
+  tableRoot.render(<Detector />)
+})()
