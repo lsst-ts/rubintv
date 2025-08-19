e5188d3874da2defa5f8e293fbeeb9e039bddd8d
ugyballoons <ugyballoons@gmail.com>
2025-08-19 13:55:51 +0100

    Use fvm to install Flutter

--- scripts/build-flutter-app.sh
+++ scripts/build-flutter-app.sh
@@ -9,10 +9,27 @@ cd /usr/src/rubintv
 git clone --single-branch --branch $DDV_DEPLOY_BRANCH https://github.com/lsst-sitcom/rubin_chart
 git clone --single-branch --branch $DDV_DEPLOY_BRANCH https://github.com/lsst-ts/rubintv_visualization ./ddv
 
+# Install Flutter version for rubin_chart using fvm
+cd rubin_chart
+if [ -f ".fvmrc" ] || [ -f "fvm_config.json" ]; then
+    fvm install
+    fvm use
+fi
+cd ..
+
 # Base HREF must be bookended by "/".
 DDV_CLIENT_WS_ADDRESS=${DDV_CLIENT_WS_ADDRESS:-rubintv/ws/ddv}
 DDV_BASE_HREF=${DDV_BASE_HREF:-/rubintv/ddv/}
 
 cd ddv
 echo "ADDRESS=$DDV_CLIENT_WS_ADDRESS" > .env
-flutter build web --base-href $DDV_BASE_HREF --profile --source-maps
+
+# Install and use Flutter version specified in fvm_config.json
+if [ -f ".fvmrc" ] || [ -f "fvm_config.json" ]; then
+    fvm install
+    fvm use
+    fvm flutter build web --base-href $DDV_BASE_HREF --profile --source-maps
+else
+    # Fallback to global flutter if no fvm config found
+    flutter build web --base-href $DDV_BASE_HREF --profile --source-maps
+fi
